95b12ad5e7732fd2601214219d6dbfb5189ea962
pengchao <pengchao.ma2@outlook.com>
2025-09-15 17:30:12 +0800

    modified again

--- terraform-karpenter-module/karpenter-provisioner.tf
+++ terraform-karpenter-module/karpenter-provisioner.tf
@@ -1,26 +1,8 @@
-resource "kubernetes_manifest" "karpenter_awsnodetemplate" {
-  provider = kubernetes.eks
-
-  depends_on = [
-    helm_release.karpenter
-  ]
-
-  manifest = {
-    apiVersion = "karpenter.sh/v1alpha5"
-    kind       = "AWSNodeTemplate"
-    metadata   = { name = "default" }
-    spec = {
-      subnetSelector        = { "kubernetes.io/cluster/${var.cluster_name}" = "owned" }
-      securityGroupSelector = { "kubernetes.io/cluster/${var.cluster_name}" = "owned" }
-    }
-  }
-}
-
 resource "kubernetes_manifest" "karpenter_provisioner" {
   provider = kubernetes.eks
 
   depends_on = [
-    kubernetes_manifest.karpenter_awsnodetemplate
+    kubernetes_manifest.karpenter_nodeclass
   ]
 
   manifest = {
@@ -38,7 +20,7 @@ resource "kubernetes_manifest" "karpenter_provisioner" {
       limits = {
         resources = { cpu = "1000" }
       }
-      providerRef          = { name = kubernetes_manifest.karpenter_awsnodetemplate.manifest[0].metadata.name }
+      providerRef          = { name = kubernetes_manifest.karpenter_nodeclass.manifest[0].metadata.name }
       ttlSecondsAfterEmpty = var.karpenter_ttl_seconds_after_empty
     }
   }
