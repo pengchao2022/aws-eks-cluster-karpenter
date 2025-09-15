34020284cbb2a60a8c9dfe16c8f1d0a33b4f791e
pengchao <pengchao.ma2@outlook.com>
2025-09-15 18:13:12 +0800

    modified

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+##################################################
+# 调用 Karpenter 模块
+##################################################
+module "karpenter" {
+  source       = "./terraform-karpenter-module" # 模块路径，根据实际情况修改
+  aws_region   = "us-east-1"
+  cluster_name = "spring-eks-cluster"
+
+  karpenter_instance_types          = ["t3.medium"]
+  karpenter_ttl_seconds_after_empty = 60
+}
+
+##################################################
+# 输出模块信息
+##################################################
 output "karpenter_iam_role_arn" {
   value = module.karpenter.karpenter_iam_role_arn
 }
