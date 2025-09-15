fffa8ae6937fa367451ffd680176b7ed1a20e338
pengchao <pengchao.ma2@outlook.com>
2025-09-15 18:03:38 +0800

    modified

--- terraform-karpenter-module/outputs.tf
+++ terraform-karpenter-module/outputs.tf
@@ -1,3 +1,15 @@
+output "karpenter_iam_role_arn" {
+  value = aws_iam_role.karpenter.arn
+}
+
+output "karpenter_helm_release_name" {
+  value = helm_release.karpenter.name
+}
+
+output "karpenter_namespace" {
+  value = helm_release.karpenter.namespace
+}
+
 output "karpenter_provisioner_name" {
   value = kubernetes_manifest.karpenter_provisioner.manifest[0].metadata.name
 }
