0702d1fe89a80828698237a0a133e7b09128bc16
pengchao <pengchao.ma2@outlook.com>
2025-09-15 18:11:18 +0800

    modified

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-module "karpenter" {
-  source       = "./terraform-karpenter-module"
-  aws_region   = "us-east-1"
-  cluster_name = "spring-eks-cluster"
-
-  karpenter_instance_types          = ["t3.micro"]
-  karpenter_ttl_seconds_after_empty = 60
-}
-
 output "karpenter_iam_role_arn" {
   value = module.karpenter.karpenter_iam_role_arn
 }
