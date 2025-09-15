8368d825f9be519e5b5d1525c3774da035d980c2
pengchao <pengchao.ma2@outlook.com>
2025-09-15 16:06:58 +0800

    modified again

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 module "karpenter" {
-  source       = "./terraform-karpenter-module" # 指向你的模块目录
-  aws_region   = "us-east-1"
-  cluster_name = "spring-eks-cluster"
-
-  # 可选：自定义实例类型和 TTL
+  source                            = "./terraform-karpenter-module"
+  aws_region                        = "us-east-1"
+  cluster_name                      = "spring-eks-cluster"
   karpenter_instance_types          = ["t3.micro"]
   karpenter_ttl_seconds_after_empty = 30
+}
+
+output "karpenter_iam_role_arn" {
+  value = module.karpenter.karpenter_iam_role_arn
+}
+
+output "karpenter_provisioner_name" {
+  value = module.karpenter.karpenter_provisioner_name
 }
