4a151d11c24e1dbb952a2d7393d4c67471549f9c
pengchao <pengchao.ma2@outlook.com>
2025-09-15 17:52:32 +0800

    updated

--- main.tf
+++ main.tf
@@ -7,11 +7,22 @@ module "karpenter" {
   karpenter_ttl_seconds_after_empty = 60
 }
 
-
 output "karpenter_iam_role_arn" {
   value = module.karpenter.karpenter_iam_role_arn
 }
 
+output "karpenter_helm_release_name" {
+  value = module.karpenter.karpenter_helm_release_name
+}
+
+output "karpenter_namespace" {
+  value = module.karpenter.karpenter_namespace
+}
+
 output "karpenter_provisioner_name" {
   value = module.karpenter.karpenter_provisioner_name
+}
+
+output "karpenter_nodeclass_name" {
+  value = module.karpenter.karpenter_nodeclass_name
 }
